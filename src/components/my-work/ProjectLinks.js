1808b988080f001f93acd8d09678393e81813f41
Fabien <depasse.fabien@gmail.com>
2021-04-04 20:03:18 +0100

    animations

--- src/components/my-work/ProjectLinks.js
+++ src/components/my-work/ProjectLinks.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 function ProjectLinks({ project }) {
   return <nav>
-    <figure>
-      <a href={project.url} target='blank'>
+    <a href={project.url} target='blank'>
+      <figure>
         <i className='fa fa-link fa-2x' aria-label='link to project'></i>
-      </a>
-      <figcaption>URL</figcaption>
-    </figure>
-    <figure>
-      <a href={project.github} target='blank'>
+        <figcaption>URL</figcaption>
+      </figure>
+    </a>
+    <a href={project.github} target='blank'>
+      <figure>
         <i className='fa fa-github fa-2x' aria-label='link to github'></i>
-      </a>
-      <figcaption>README</figcaption>
-    </figure>
+        <figcaption>README</figcaption>
+      </figure>
+    </a>
   </nav>
 }
 

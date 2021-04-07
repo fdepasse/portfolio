76a52e0c5e5e37ad9532c2520cfaaddead48ff40
Fabien <depasse.fabien@gmail.com>
2021-04-07 18:24:17 +0100

    small devices

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -5,19 +5,18 @@ import { SiGithub, SiGmail, SiLinkedin } from 'react-icons/si'
 function NavBar() {
   return <header>
     <nav id='navbar'>
-      <a href='#about'>ABOUT ME</a>
-      {/* <a href='#timeline'>TIMELINE</a> */}
-      <a href='#mywork'>MY WORK</a>
-      <a href='#skills'>SKILLS</a>
-      <a href='#contact'>CONTACT</a>
-      <a href='mailto:depasse.fabien@gmail.com' aria-label='link to send an email'>
-        <SiGmail className='icon'/>
+        <a href='#about'>ABOUT ME</a>
+        <a href='#mywork'>MY WORK</a>
+        <a href='#skills'>SKILLS</a>
+        <a href='#contact'>CONTACT</a>
+        <a href='mailto:depasse.fabien@gmail.com' aria-label='link to send an email'>
+        <SiGmail className='icon' />
       </a>
       <a href='https://github.com/fdepasse' target='blank' aria-label='link to github profile'>
-        <SiGithub className='icon'/>
+        <SiGithub className='icon' />
       </a>
       <a href='https://www.linkedin.com/in/depassefabien/' target='blank' aria-label='link to linkedin profile'>
-        <SiLinkedin className='icon'/>
+        <SiLinkedin className='icon' />
       </a>
     </nav>
   </header>

7af81b49d3593b468f2c58c03ea689674f9be098
Fabien <depasse.fabien@gmail.com>
2021-04-14 18:55:43 +0100

    navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return <section id='home' className='section'>
       <h1 className='appear-left'>FABIEN DEPASSE</h1>
       <h2 className='appear-right'>Junior Developer</h2>
-      <a href='#about'className='appear-left'><i className='arrow down'></i></a>
+      <a href='#about'className='appear-left bounce'><i className='arrow down'></i></a>
     <span id='credits'>Photo by <a href="https://unsplash.com/@bundo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target='_blank' rel='noreferrer'>Bundo Kim</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText "target='_blank' rel='noreferrer'>Unsplash</a></span>
   </section>
 }
